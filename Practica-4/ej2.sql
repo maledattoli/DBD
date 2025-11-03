34be248c3450077d3a006976c44f9ec274940d7a
Malena Carrasco Dattoli <malenacdattoli@gmail.com>
2025-11-03 00:42:24 -0300

    agrego lo que quedaba del ej 2

--- Practica-4/ej2.sql
+++ Practica-4/ej2.sql
@@ -61,4 +61,64 @@ where exists(select pd.nroArbol
 from Poda pd 
 where (pd.nroArbol = a.nroArbol) and (pd.fecha Between '2023-01-01' AND '2023-12-31' )) )
 
-/* */
+/*5. Reportar DNI, nombre, apellido, fecha de nacimiento y localidad donde viven de aquellos
+podadores con apellido terminado con el string ‘ata’ y que tengan al menos una poda durante
+2024. Ordenar por apellido y nombre*/
+/*---->>??? */ 
+((SELECT pdd.dni, pdd.nombre, pdd.apellido, pdd.fnac, l.nombreL
+FROM Localidad l inner Join Podador pdd on (l.codigoPostal = pdd.codigoPostalVive )
+inner join Poda pd on ( pdd.dni = pd.dni)
+where pdd.apellido LIKE '%ata')
+INTERSECT
+(SELECT pdd.dni, pdd.nombre, pdd.apellido, pdd.fnac, l.nombreL
+FROM Localidad l inner Join Podador pdd on (l.codigoPostal = pdd.codigoPostalVive )
+inner join Poda pd on ( pdd.dni = pd.dni)
+where pd.fecha Between '2024-01-01' AND '2024-12-31'))
+ORDER BY apellido, nombre
+
+/*OR ---->>??? */ 
+SELECT pdd.dni, pdd.nombre, pdd.apellido, pdd.fnac, l.nombreL
+FROM Podador pdd
+JOIN Localidad l ON (l.codigoPostal = pdd.codigoPostalVive)
+WHERE pdd.apellido LIKE '%ata'
+  AND EXISTS (
+    SELECT pd.dni 
+    FROM Poda pod
+    WHERE (pd.dni = pdd.dni) AND
+    (pd.fecha BETWEEN '2024-01-01' AND '2024-12-31')
+  )
+ORDER BY pdd.apellido, pdd.nombre;
+
+/*6. Listar DNI, apellido, nombre, teléfono y fecha de nacimiento de podadores que solo podaron
+árboles de especie ‘Coníferas’*/
+SELECT pdd.dni, pdd.apellido, pdd.nombre, pdd.telefono, pdd.fnac
+FROM Arbol a inner JOIN Poda pd on (a.nroArbol = pd.nroArbol)
+inner JOIN Podador pdd on (pd.dni = pdd.dni)
+where a.especie = 'Cornífera'
+Except /*hace el distinct innecesario no---->>??? */ 
+SELECT pdd.dni, pdd.apellido, pdd.nombre, pdd.telefono, pdd.fnac
+FROM Arbol a inner JOIN Poda pd on (a.nroArbol = pd.nroArbol)
+inner JOIN Podador pdd on (pd.dni = pdd.dni)
+where a.especie <> 'Cornífera'
+
+/*7. Listar especies de árboles que se encuentren en la localidad de ‘La Plata’ y también en la
+localidad de ‘Salta’.*/
+(Select a.especie
+from Localidad l inner join Arbol a on (l.codigoPostal = a.codigoPostal)
+where l.nombreL = 'La Plata')
+intersect
+(Select a.especie
+from Localidad l inner join Arbol a on (l.codigoPostal = a.codigoPostal)
+where l.nombreL = 'Salta')
+
+/* 8. Eliminar el podador con DNI 22234566*/ 
+Delete from Podador 
+where dni = 22234566;
+
+/*9. Reportar nombre, descripción y cantidad de habitantes de localidades que tengan menos de 5
+árboles*/ 
+Select l.nombreL, l.descripcion, l.nroHabitantes
+from Localidad l 
+inner join Arbol a on (l.codigoPostal = a.codigoPostal)
+group by l.nombreL, l.descripcion, l.nroHabitantes
+having COUNT(a.nroArbol) < 5;
---
+++ Practica-4/ej3.sql
@@ -0,0 +1,27 @@
+/* Banda = (-codigoB-, nombreBanda, genero_musical, anio_creacion)
+Integrante = (-DNI-, nombre, apellido, dirección, email, fecha_nacimiento, codigoB(fk))
+Escenario = (-nroEscenario-, nombre_escenario, ubicación, cubierto, m2, descripción)
+Recital = (-fecha, hora, nroEscenario (fk)-, codigoB (fk))
+*/
+/* 1. Listar DNI, nombre, apellido,dirección y email de integrantes nacidos entre 1980 y 1990 y que
+hayan realizado algún recital durante 2023.
+*/
+
+/* 2. Reportar nombre, género musical y año de creación de bandas que hayan realizado recitales
+durante 2023, pero no hayan tocado durante 2022 .
+*/
+/* 3. Listar el cronograma de recitales del día 04/12/2023. Se deberá listar nombre de la banda que
+ejecutará el recital, fecha, hora, y el nombre y ubicación del escenario correspondiente.*/
+/* 4. Listar DNI, nombre, apellido,email de integrantes que hayan tocado en el escenario con nombre
+‘Gustavo Cerati’ y en el escenario con nombre ‘Carlos Gardel’*/
+/* 5. Reportar nombre, género musical y año de creación de bandas que tengan más de 5 integrantes.
+*/
+/*6. Listar nombre de escenario, ubicación y descripción de escenarios que solo tuvieron recitales
+con el género musical rock and roll. Ordenar por nombre de escenario
+ */
+/*7. Listar nombre, género musical y año de creación de bandas que hayan realizado recitales en
+escenarios cubiertos durante 2023.// cubierto es true, false según corresponda */
+/*8. Reportar para cada escenario, nombre del escenario y cantidad de recitales durante 2024. */
+/* 9. Modificar el nombre de la banda ‘Mempis la Blusera’ a: ‘Memphis la Blusera’.
+*/
+/* */
